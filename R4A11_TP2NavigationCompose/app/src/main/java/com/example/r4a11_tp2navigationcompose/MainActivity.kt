7b7a1f8875f09ee986c7afee46c0e429d1a2ac74
AzarioCossa <cossaazario@gmail.com>
2025-02-05 17:18:23 +0100

    Second page finalization

--- R4A11_TP2NavigationCompose/app/src/main/java/com/example/r4a11_tp2navigationcompose/MainActivity.kt
+++ R4A11_TP2NavigationCompose/app/src/main/java/com/example/r4a11_tp2navigationcompose/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,8 +20,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -31,9 +34,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.r4a11_tp2navigationcompose.ui.theme.R4A11_TP2NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,7 +67,19 @@ fun AppNavigation() {
         composable("form") {
             FormScreen(navController = navController)
         }
+        composable(
+            route = "display/{name}",
+            arguments = listOf(navArgument("name") { defaultValue = ""})
+        ){backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name")?:""
+            DisplayScreen(navController=navController, name)
+        }
     }
+}
+
+@Composable
+fun DisplayScreen(navController: NavHostController, name: String) {
+        Text("Affichage du formulaire")
 }
 
 @Composable
@@ -79,8 +96,22 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun FormScreen(navController: NavController) {
+    var name by remember { mutableStateOf("") }
+
     Column(modifier = Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Page du formulaire")
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = name,
+            onValueChange = { newText -> name = newText },
+            label={Text("Entrez votre nom")},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+                .padding(top = 50.dp).fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("display/$name") }) {
+            Text("Valider")
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { navController.navigate("home") }) {
             Text("Retour")
