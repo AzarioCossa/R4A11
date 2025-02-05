a7a377303086c3c6c25ddfac226055d8994849ed
AzarioCossa <cossaazario@gmail.com>
2025-02-05 16:58:18 +0100

    Jetpack Compose Navigation conclusion

--- R4A11_TP2NavigationCompose/app/src/main/java/com/example/r4a11_tp2navigationcompose/MainActivity.kt
+++ R4A11_TP2NavigationCompose/app/src/main/java/com/example/r4a11_tp2navigationcompose/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.example.r4a11_tp2navigationcompose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.r4a11_tp2navigationcompose.ui.theme.R4A11_TP2NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +42,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             R4A11_TP2NavigationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MaterialTheme{
+                    AppNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    R4A11_TP2NavigationComposeTheme {
-        Greeting("Android")
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+        composable("form") {
+            FormScreen(navController = navController)
+        }
     }
 }
+
+@Composable
+fun HomeScreen(navController: NavController) {
+   Column(modifier = Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally)
+   {
+         Text("Bienvenue dans ma première application commpose navigation")
+        //style =  MaterialTheme.typography.titleMedium
+         Button(onClick = { navController.navigate("form") }) {
+              Text("Accéder au formulaire")
+         }
+   }
+}
+
+@Composable
+fun FormScreen(navController: NavController) {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Page du formulaire")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("home") }) {
+            Text("Retour")
+        }
+    }
+}
+
+
